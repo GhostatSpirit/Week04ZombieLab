7073f7ceb2d1b08886aedd9775f98f516c9bb849
Yang Liu <Yang Liu>
2016-07-27 12:33:54 -0400

    Added showHealth().

---
+++ Assets/Scripts/PrintHealth.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PrintHealth : MonoBehaviour {
+
+	public GameObject player;
+
+	Text textObj;
+
+	// Use this for initialization
+	void Start () {
+		textObj = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int health;
+		if(player == null){
+			health = 0;
+		} else {
+			health = player.GetComponent<Killable>().currentHealth;
+		}
+
+		textObj.text = "Health: " + health.ToString ();
+
+	}
+}
